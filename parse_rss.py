bb358f7db2fcd1d210a50cb43f217317696ff568
Felipe Lacerda <fegolac@gmail.com>
2015-12-01 22:35:32 +0100

    mark tweets as tweeted when tweeted. Also, use actual URL instead of Google's redirect

--- parse_rss.py
+++ parse_rss.py
@@ -6,9 +6,14 @@ def parse_rss2(file_desc):
     tree = etree.parse(file_desc)
     root = tree.getroot()
     items = root.find("channel").findall("item")
-    entries = [{"title": i.find("title").text,
-                "url": i.find("link").text,
-                "guid": i.find("guid").text} for i in items]
+    entries = []
+    for i in items:
+        title = i.find("title").text
+        guid = i.find("guid").text
+        link = i.find("link").text
+        link_split = dict([tuple(i.split("=")) for i in link.split("&")])
+        url = link_split["url"]
+        entries.append({"title": title, "url": url, "guid": guid})
     return entries
 
 def create_db(db):
@@ -38,5 +43,8 @@ def push_db(entries, db):
 def choose_tweet(db):
     conn = sqlite3.connect(db)
     c = conn.cursor()
-    c.execute("SELECT title, url FROM entries WHERE tweeted = 0")
+    c.execute("SELECT title, url, id FROM entries WHERE tweeted = 0")
+    tweet = c.fetchone()
+    if tweet is not None:
+        c.execute("UPDATE entries SET tweeted = 1 WHERE id = ?", tweet[2])
     return c.fetchone()
